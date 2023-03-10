b72b70aaeee94b953afa00528c2dacccc0711e08
debora-be <arobedbee@gmail.com>
2023-03-10 14:55:49 -0300

    add: compojure, ring, http-kit and data.json  deps

--- project.clj
+++ project.clj
@@ -3,7 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]] 
+                 ; Compojure - A basic routing library
+                 [compojure "1.7.0"]
+                 ; Our Http library for client/server
+                 [http-kit "2.6.0"]
+                 ; Ring defaults - for query params etc
+                 [ring "1.9.6"]
+                 ; Clojure data.JSON library
+                 [org.clojure/data.json "2.4.0"]]
   :main ^:skip-aot clojure-star.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
